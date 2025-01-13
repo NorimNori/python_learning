9c71e7a0c0bff57f28cb6d8bf53a5805b8a9091a
Gloria R <norimaqidoe@hotmail.com>
2025-01-12 20:23:49 -0600

    feat: booleans

---
+++ booleans.py
@@ -0,0 +1,22 @@
+### Tipo de dato que toma dos valores Verdadero o Falso (True o False).
+a = True
+b = False
+
+print(5 > 3)
+
+### Nos ayudarán a tomar decisiones.
+
+### Estructuras que devuelven verdadero (las que tienen contenido).
+string = bool("No se olviden de suscribirse al canal")
+num = bool(2025)
+lista = bool(['Uva', 'Durazno'])
+dict = bool({"nombre": "Hugo"})
+
+### Estructuras que devuelven falso (todas están vacías de contenido).
+string2 = bool("")
+num2 = bool(0)
+lista2 = bool([])
+dict2 = bool({})
+# En otros lenguajes se conoce como null.
+none = bool(None)
+
