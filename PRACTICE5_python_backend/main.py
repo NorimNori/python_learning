b3e34edb7e2b02cec6108a473a44d4a3cde36ee4
Gloria R <norimaqidoe@hotmail.com>
2025-01-16 01:26:45 -0600

    feat: Practice APIRest, creating the methods to delete and update and importing List in get (all)

--- PRACTICE5_python_backend/main.py
+++ PRACTICE5_python_backend/main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 import uuid
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -20,7 +20,7 @@ courses_db = []
 # CRUD
 
 ## Read con GET (all).
-@app.get('/courses/', response_model = Course)
+@app.get('/courses/', response_model = List[Course])
 def get_courses():
     return courses_db
 
@@ -40,3 +40,25 @@ def create_course(course: Course):
     course.id = str(uuid.uuid4())
     courses_db.append(course)
     return course
+
+## Update con PUT.
+@app.put('/courses/{id}', response_model = Course)
+def update_course(id:str, course_updated:Course):
+    course = next((course for course in courses_db if course.id == id), None)
+    if course is None:
+        raise HTTPException(status_code = 404, detail = 'course not found')
+    course_updated.id = id
+    ## Buscamos en indice exacto del curso a actualizar con index() en la base de datos (courses_db).
+    index = courses_db.index(course)
+    courses_db[index] = course_updated
+    return course_updated
+
+## Eliminar con DELETE.
+@app.delete('/courses/{id}', response_model = Course)
+def delete_course(id:str):
+    course = next((course for course in courses_db if course.id == id), None)
+    if course is None:
+        raise HTTPException(status_code = 404, detail = 'course not found')
+    courses_db.remove(course)
+    return course
+
