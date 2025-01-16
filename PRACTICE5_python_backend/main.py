ebc9157f021c87860c0dae989ba323a6665617a6
Gloria R <norimaqidoe@hotmail.com>
2025-01-16 00:14:44 -0600

    feat: Practice APIRest, creation of the get all, get by id and post methods

--- PRACTICE5_python_backend/main.py
+++ PRACTICE5_python_backend/main.py
@@ -1,5 +1,6 @@
 from typing import Optional
-from fastapi import FastAPI
+import uuid
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 # Inicializacion de variable con caracteristicas de una API REST.
@@ -15,3 +16,27 @@ class Course(BaseModel):
 
 # Simulacion de la base de datos.
 courses_db = []
+
+# CRUD
+
+## Read con GET (all).
+@app.get('/courses/', response_model = Course)
+def get_courses():
+    return courses_db
+
+## Read con Get (id).
+@app.get('/courses/{id}', response_model = Course)
+def get_course(id:str):
+    ## next() toma la primera coincidencia del array devuelto.
+    course = next((course for course in courses_db if course.id == id), None)
+    if course is None:
+        raise HTTPException(status_code = 404, detail = 'course not found')
+    return course
+
+## Create con POST.
+@app.post('/courses/', response_model = Course)
+def create_course(course: Course):
+    ## uuid se encarga de generar un id unico.
+    course.id = str(uuid.uuid4())
+    courses_db.append(course)
+    return course
